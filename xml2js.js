1840cbb9f277bb332f2701d0e4e1bc0d27fee804
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2025-05-25 01:38:42 +0200

    Add XML parsing functionality and dependencies
    - Created package.json to manage project dependencies, including xml2js.
    - Added package-lock.json to lock dependency versions.
    - Implemented xml2js.js to read an XML file, parse it, and export the result as a JavaScript module.

---
+++ xml2js.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const xml2js = require("xml2js");
+
+// read your XML file
+const xml = fs.readFileSync("icons.xml", "utf8");
+
+// configure parser
+const parser = new xml2js.Parser({
+  explicitArray: false, // don’t wrap every element in an array
+  mergeAttrs: true, // bring attributes up into the object
+});
+
+parser.parseString(xml, (err, result) => {
+  if (err) throw err;
+
+  fs.writeFileSync(
+    "icons.js",
+    `export default ${JSON.stringify(result.rules.rule, null, 2)};`
+  );
+});
